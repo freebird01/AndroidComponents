5929fb3a80027b2c3f8100c4dd6b3a4359152b7b
Nikhil Arora <nikhilarora@adobe.com>
2024-12-23 18:01:41 +0530

    Capture user input and display it

--- app/src/main/java/com/example/androidcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidcomponents/MainActivity.kt
@@ -12,18 +12,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,6 +84,10 @@ fun Components(name: String, modifier: Modifier = Modifier) {
         mutableStateOf("")
     }
 
+    val userInput = remember {
+        mutableStateOf("Result: ")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -113,6 +124,7 @@ fun Components(name: String, modifier: Modifier = Modifier) {
                     myTextColor.value = Color.Black
                 }
                 myButtonStatus.value = !myButtonStatus.value
+                userInput.value = valueOnTextField.value
             },
             modifier = Modifier.size(250.dp, 60.dp),
             colors = ButtonDefaults.buttonColors(myBackgroundColor.value),
@@ -133,7 +145,35 @@ fun Components(name: String, modifier: Modifier = Modifier) {
             value = valueOnTextField.value,
             onValueChange = {
                 valueOnTextField.value = it
-            }
+            },
+            //Focused label
+            label = { Text("Enter Your Name", color = Color.White) },
+            modifier = Modifier
+                .width(300.dp)
+                .background(color = Color.Green),
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Green,
+                focusedIndicatorColor = Color.Yellow,
+                unfocusedIndicatorColor = Color.Red,
+                focusedLabelColor = Color.Black,
+                unfocusedLabelColor = Color.Cyan
+            ),
+//            textStyle = TextStyle(color = Color.White, background = Color.Blue)
+            maxLines = 4,
+//            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+//            visualTransformation = PasswordVisualTransformation()
+
+        )
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        Text(
+            text = userInput.value,
+            color = Color.White,
+            modifier = Modifier
+                .background(color = Color.Red, shape = RoundedCornerShape(10.dp))
+                .padding(10.dp)
         )
     }
 }
